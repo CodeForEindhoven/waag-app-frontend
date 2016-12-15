7039aacf7b38dd89933caad263638d7cfdc30b16
Marcel Goethals <marcel.goethals@gmail.com>
2016-12-15 16:33:40 +0100

    remote loading

--- src/js/main.js
+++ src/js/main.js
@@ -19,27 +19,68 @@ model.meetlatten = m.prop([
 	}
 ]);
 
-const app = {};
-app.view = function() {
-	return m('div', {class:"page"}, model.meetlatten().map((meetlat)=>{
-		return m.component(card, {
-			content: [{
-				text: {
-					content: [
-						m("div", {class: "omschrijving"}, meetlat.title),
-						m.component(slider, {
-							min: 0,
-							max: 10,
-							value: meetlat.score,
-							step: 1,
-							ticks: true,
-							pin: true
-						})
-					]
-				}
-			}]
-		});
-	}));
+var model = {
+	indicators: m.prop([]),
+	handshake: m.prop({
+		identity_id: "",
+		session_id: ""
+	})
+};
+var endpoint = "http://localhost:8080/api";
+
+function GET(url){
+	return m.request({method: "GET", url: endpoint+url});
+}
+
+function POST(url, data){
+	return m.request({method: "POST", url: endpoint+url, data: data});
+}
+
+POST("/handshake").then(model.handshake);
+GET("/indicators").then(model.indicators);
+
+const app = {
+	controller:() => {
+		return {
+			update: (id,score) => {
+				POST("/score", {
+					indicator_id: id,
+					session_id: model.handshake().session_id,
+					score: score
+				});
+			}
+		};
+	},
+
+	view:(ctrl) => {
+		return m('div', {class:"page"}, model.indicators().map((indicator)=>{
+			if(!indicator.score){
+				indicator.score = 5;
+			}
+			return m.component(card, {
+				content: [{
+					text: {
+						content: [
+							m("div", {class: "omschrijving"}, indicator.title),
+							m.component(slider, {
+								min: 0,
+								max: 10,
+								value: indicator.score,
+								step: 1,
+								ticks: true,
+								pin: true,
+								getValue: (v) => {
+									if(v !== indicator.score){
+										ctrl.update(indicator.id, v);
+									}
+								}
+							})
+						]
+					}
+				}]
+			});
+		}));
+	}
 };
 
 m.mount(document.getElementById("app"), app);
